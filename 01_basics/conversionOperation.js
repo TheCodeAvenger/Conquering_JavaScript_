73a8d570d8e7d528f20e834879e25daf87caee30
Krishna <krishnamdev282@gmail.com>
2025-10-12 21:30:35 +0530

    conversion

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,12 +1,41 @@
- let score = 24;
- console.log(score);
- 
- console.log(typeof (score));
- 
- score = "24aaa";
- console.log(typeof (score));
+//1 ----------------- in number : 
 
- let valueInNumber = Number(score); // converted "score" into number using "Number()"
 
- console.log(typeof (valueInNumber));
- console.log(valueInNumber);  // not as number (not pure number)
+//  let score = 24;
+//  console.log(score);
+ 
+//  console.log(typeof (score));
+ 
+//  score = "24aaa";
+//  console.log(typeof (score));
+
+//  let valueInNumber = Number(score); // converted "score" into number using "Number()"
+
+//  console.log(typeof (valueInNumber));
+//  console.log(valueInNumber);  // not as number (not pure number) 
+
+
+// 2------------------  into boolean :
+
+let isloggedin = "krish";
+// console.log( isloggedin);
+
+let booleanisloggedin= Boolean(isloggedin);
+// console.log(booleanisloggedin);
+
+// 1 ->true
+// 0 ->false
+
+// " " -> false
+// " krishna " -> True 
+
+
+
+// 3------------------  into string :
+
+let somenum = 24;
+
+let stringSomeNum = String(somenum);
+
+console.log(stringSomeNum);
+console.log( typeof stringSomeNum);
